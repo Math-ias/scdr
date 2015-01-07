b53cec135caeb40ba55126b2a03ba2a314cd32b2
MKSwizzle <m.kools@comcast.net>
2015-01-07 16:12:19 -0500

    Add no color options for piping, as well as being written to file
    I noted that during this commit, I changed my APIKEY however there was a gitignore for it, this might be because
    I committed the APIKEY file after I committed the .gitignore,
     I applied a solution on my computer however this might not carry over in the commit.
    
    :frowning:

--- dictionary.py
+++ dictionary.py
@@ -5,6 +5,7 @@ import urllib2
 import re
 import colorama
 import spellingCorrector as corrector
+import sys
 
 with open("APIKEY", "r") as apikeyfile:
 	key = apikeyfile.read().replace('\n', '')
@@ -27,14 +28,23 @@ def parseWord(word):
 
 if __name__ == "__main__":	
 
+	stdout = sys.stdout 
+
 	colorama.init(autoreset=True)
 
 	optionParser = OptionParser()
 	optionParser.add_option("-f", "--file", action="store", help="Take words from a specified file.", metavar="FILE")
 	optionParser.add_option("-a", "--alternating", dest="alternating", action="store_true", default = True, help="Alternate the background colors for the definitions to make them more distinguishable.")
 	optionParser.add_option("-s", "--sane", dest="alternating", action="store_false", help="Keep the output sane, no alternating background colors.")
+	optionParser.add_option("-b", "--boring", dest="color", action="store_false", default = True, help="Have the output better for piping output to files, no color, no bolding, also turns off alternating.")
+	optionParser.add_option("-o", "--output", dest="output", action="store", help="Output all the definitions to a chosen file, the boring option is also set to true.", metavar="FILE")
 	(options, args) = optionParser.parse_args()
 	words = {}
+
+	if options.output is not None:
+		sys.stdout = open(options.output, 'w')
+		options.color = False
+		options.alternating = False
 
 	if options.file is not None:
 		wordFile = open(options.file)
@@ -47,8 +57,13 @@ if __name__ == "__main__":
 
 	for word in words:
 		cleanedDefinitions = parseWord(word)
-		print colorama.Fore.RED + colorama.Style.BRIGHT + word.upper()
+		if options.color == True:
+			print colorama.Fore.RED + colorama.Style.BRIGHT + word.upper()
+		else:
+			print word.upper()
 		for num,definition in enumerate(cleanedDefinitions):
-			definition = colorama.Back.GREEN + definition if num % 2 == 0 and options.alternating else definition
-			print "%d -> %s " % (num+1, definition)
-			
+			if num % 2 == 0 and options.alternating == True and options.color == True:
+				definition = colorama.Back.GREEN + definition
+			print ("%d -> %s " % (num+1, definition))
+	
+	sys.stdout = stdout
