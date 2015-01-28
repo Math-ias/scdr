666049e21cf45bd45b9224ec42311185c4e55abe
MKSwizzle <m.kools@comcast.net>
2015-01-28 17:55:42 -0500

    Fix python3 conversion and overhaul code
    There are a lot of updates in this commit, sadly without each change,
    there could be no working commit, it had to happen all at once. In this
    commit the etree package is no longer used and instead the lxml etree package
    is used, this is because it's a better library and python3 was having difficulty
    with the normal etree package.
    The second major change is that, since python3 needs it, urllib was changed to
    the package urllib.request.
    The third major change to keep up with the python3 compatability was the swapping of
    OptionParser for argparser, this change was small but is worth a note.
    
    In summary the big changes were:
    * use of lxml
    * use of urllib.request
    * use of argparser
    
    Among that are fixes for things that were breaking in the previous commit.

--- dictionary.py
+++ dictionary.py
@@ -1,38 +1,34 @@
-import xml.etree.ElementTree as ET # Hey! ET!
-from optparse import OptionParser
-import urllib
+from lxml import etree as lxml
+import argparse
 import re
 import colorama
 import spellingCorrector as corrector
 import sys
+
+try:
+	input = raw_input
+except NameError:
+	pass # Just a very rough attempt at python2 & 3 compatability
+
 try:
 	with open("APIKEY", 'r') as apikeyfile:
 		key = apikeyfile.read().replace('\n', '')
 except IOError as e:
-	key = input("Your APIKEY file does not exist! Please input your apikey for the merriam webster student dictionary below:")
+	key = input("Your APIKEY file does not exist! Please input your apikey for the merriam webster student dictionary:")
 	with open("APIKEY", 'w') as apikeyfile:
 		apikeyfile.write(key)
 
 def parseWord(word):
 	try:
-		merriamXML = urllib.urlopen("http://www.dictionaryapi.com/api/v1/references/sd4/xml/{0}?key={1}".format(word, key))
-	except urllib.error.URLError as urlerror:
-		print("There was an error in the URL. {1}".format(urlerror.reason))	
-		return {"Could not retrieve definition due to an error."}
-
-	try:
-		tree = ET.parse(merriamXML)	
-		root = tree.getroot()
+		tree = lxml.parse("http://www.dictionaryapi.com/api/v1/references/sd4/xml/{0}?key={1}".format(word, key))
 		defs = tree.findall(".//dt")
-	except ET.ParseError as pe: # NOTE: Due to bad documentation I could not find much about ParseError and if it's even thrown, it's in the source code of etree so I can check for it, but at this time it is unknown whether or not this is needed.
+	except lxml.ParseError as pe:
 		print("There was an error when parsing the files from merriam webster.")
 		return {"Could not recieve definitions due to an error involving parsing."}
 	
-	# Merriam throws these disgusting extra tags in there. :(
-	# So we got to clean it up.	
-	
-	definitions = [re.sub(r'<[^>]*?>', '', ET.tostring(definition)) for definition in defs] # Error handling here?	
-	# Shortened version of for def in defs: definitions.append(re.blahblahblah, stuff)
+	# Strip text of unparsed tags.
+
+	definitions = [defin.text for defin in defs]	
 
 	return definitions
 
@@ -42,38 +38,41 @@ if __name__ == "__main__":
 
 	colorama.init(autoreset=True)
 
-	optionParser = OptionParser()
-	optionParser.add_option("-f", "--file", action="store", help="Take words from a specified file.", metavar="FILE")
-	optionParser.add_option("-a", "--noalternating", dest="alternating", action="store_false", default=True, help="Keep the output sane, no alternating background colors.")
-	optionParser.add_option("-b", "--noformat", dest="color", action="store_false", default = True, help="Remove all formatting from the text, useful for piping.")
-	optionParser.add_option("-o", "--output", dest="output", action="store", help="Output all the definitions to a chosen file, the boring option is also set to true.", metavar="FILE")
-	(options, args) = optionParser.parse_args()
+	argParser = argparse.ArgumentParser(description="A python program to fetch English definitions from the command line.")
+	argParser.add_argument("-f", "--file", action="store", help="Take words from a specified file.", metavar="FILE")
+	argParser.add_argument("--noalternating", dest="alternating", action="store_false", default=True, help="Keep the output sane, no alternating background colors.")
+	argParser.add_argument("-n","--noformat", dest="color", action="store_false", default = True, help="Remove all formatting from the text, useful for piping.")
+	argParser.add_argument("-o","--output", dest="output", action="store", help="Output all the definitions to a chosen file, the boring option is also set to true.", metavar="FILE")
+	argParser.add_argument("words", nargs="+")
+	
+	args = argParser.parse_args()
+	
 	words = {}
 
-	if options.output is not None:
-		sys.stdout = open(options.output, 'w') # Ugly hack to redirect output of the program! Eww!
-		options.color = False
-		options.alternating = False
+	if args.output is not None:
+		sys.stdout = open(args.output, 'w') # Ugly hack to redirect output of the program! Eww!
+		args.color = False
+		args.alternating = False
 
-	if options.file is not None:
+	if args.file is not None:
 		try:
-			with open(options.file, 'r') as wordFile:
+			with open(args.file, 'r') as wordFile:
 				words = [line.rstrip() for line in wordFile.readlines()]
 		except IOError as e:
 			print("Unable to read from the given wordfile! Check that it exists and/or that there is permissions enough to access it.")
 	else:
-		words = args
+		words = args.words
 
 	words = [corrector.correct(word) for word in words]
 
 	for word in words:
 		cleanedDefinitions = parseWord(word)
-		if options.color == True:
+		if args.color == True:
 			print(colorama.Fore.RED + colorama.Style.BRIGHT + word.upper())
 		else:
 			print(word.upper())
 		for num,definition in enumerate(cleanedDefinitions):
-			if num % 2 == 0 and options.alternating == True and options.color == True:
+			if num % 2 == 0 and args.alternating == True and args.color == True:
 				definition = colorama.Back.GREEN + definition
 			print ("{0} -> {1}".format(num+1, definition))
 	
