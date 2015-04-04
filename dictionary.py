750af521c5434721963e3f232fdf420a37ec513c
Math-ias <m.kools@comcast.net>
2015-04-04 10:17:52 -0400

    Fix auto-indented code to be correct

--- dictionary.py
+++ dictionary.py
@@ -13,54 +13,51 @@ except NameError:
 try:
     with open("APIKEY", 'r') as apikeyfile:
         key = apikeyfile.read().replace('\n', '')
-    except IOError as e:
-        key = input("Your APIKEY file does not exist! Please input your apikey for Meriam-Webster Inc.'s API:")
+except IOError:
+    key = input("Your APIKEY file does not exist! Please input your apikey for Meriam-Webster Inc.'s API:")
     with open("APIKEY", 'w') as apikeyfile:
-apikeyfile.write(key)
+        apikeyfile.write(key)
 
-    class CollectorParser(object):
-        def __init__(self):
+class CollectorParser(object):
+    def __init__(self):
+        self.definition = ""
+        self.definitions = []
+
+    def start(self, tag, attrib):
+        if tag == "sx":
+            self.definition += "\nSynonym: "
+        elif tag == "dt":
             self.definition = ""
-            self.definitions = []
+        elif tag == "un":
+            self.definition += "\nUsage: "
+        elif tag == "vi":
+            self.definition += "\nExample: "
 
-            def start(self, tag, attrib):
-                if tag == "sx":
-                self.definition += "\nSynonym: "
-                elif tag == "dt":
-                self.definition = ""
-                elif tag == "un":
-                self.definition += "\nUsage: "
-                elif tag == "vi":
-                self.definition += "\nExample: "
-                def end(self, tag):
-                    if tag == "dt":
-    reformattedDefinition = self.definition[:1].upper() + self.definition[1:] # Makes the first letter uppercase.
-    reformattedDefinition = reformattedDefinition.lstrip() # Strip newlines without text (for when a definition is just a synonym)
-                    self.definitions.append(reformattedDefinition)
-                    def data(self, data):
-                        self.definition += re.sub(':', '', data)
+    def end(self, tag):
+            if tag == "dt":
+                reformattedDefinition = self.definition[:1].upper() + self.definition[1:] # Makes the first letter uppercase.
+                reformattedDefinition = reformattedDefinition.lstrip() # Strip newlines without text (for when a definition is just a synonym)
+                self.definitions.append(reformattedDefinition)
 
+    def data(self, data):
+        self.definition += re.sub(':', '', data)
 
-                        def close(self):
-                            return self.definitions	
+    def close(self):
+        return self.definitions
 
-                            def parseWord(word):
-                                try:
-                                parser = etree.XMLParser(target = CollectorParser())
-                                definitions = etree.parse("http://www.dictionaryapi.com/api/v1/references/sd4/xml/{0}?key={1}".format(word, key), parser)
-                                except etree.ParseError as pe:
-                                print("There was an error when parsing the downloaded xml.")
-                                return ["Could not recieve definitions due to an error involving parsing."]
+def parseWord(word):
+    try:
+        parser = etree.XMLParser(target = CollectorParser())
+        definitions = etree.parse("http://www.dictionaryapi.com/api/v1/references/sd4/xml/{0}?key={1}".format(word, key), parser)
+    except etree.ParseError as pe:
+        print("There was an error when parsing the downloaded xml.")
+        return ["Could not recieve definitions due to an error involving parsing."]
 
-# Strip text of unparsed tags.
+    return definitions
 
-                                return definitions
-
-                                if __name__ == "__main__":	
-
-                                stdout = sys.stdout 
-
-colorama.init(autoreset=True)
+if __name__ == "__main__":
+    stdout = sys.stdout
+    colorama.init(autoreset=True)
 
     argParser = argparse.ArgumentParser(description="A python program to fetch English definitions from the command line.")
     argParser.add_argument("-f", "--file", action="store", help="Take words from a specified file.", metavar="FILE")
@@ -69,35 +66,36 @@ colorama.init(autoreset=True)
     argParser.add_argument("-o","--output", dest="output", action="store", help="Output all the definitions to a chosen file, the boring option is also set to true.", metavar="FILE")
     argParser.add_argument("words", nargs="*")
 
-args = argParser.parse_args()
+    args = argParser.parse_args()
 
     words = {}
 
     if args.output is not None:
-    sys.stdout = open(args.output, 'w') # Ugly hack to redirect output of the program! Eww!
-    args.color = False
-    args.alternating = False
+        sys.stdout = open(args.output, 'w') # Ugly hack to redirect output of the program! Eww!
+        args.color = False
+        args.alternating = False
 
     if args.file is not None:
-    try:
-    with open(args.file, 'r') as wordFile:
-    words = [line.rstrip() for line in wordFile.readlines()]
-    except IOError as e:
-    print("Unable to read from the given wordfile! Check that it exists and/or that there is permissions enough to access it.")
+        try:
+            with open(args.file, 'r') as wordFile:
+                words = [line.rstrip() for line in wordFile.readlines()]
+        except IOError as e:
+            print("Unable to read from the given wordfile! Check that it exists and/or that there is permissions enough to access it.")
     else:
-    words = args.words
+        words = args.words
 
-    words = [corrector.correct(word) for word in words]
+        words = [corrector.correct(word) for word in words]
 
     for word in words:
-cleanedDefinitions = parseWord(word)
-    if args.color == True:
-print(colorama.Fore.RED + colorama.Style.BRIGHT + word.upper())
-    else:
-print(word.upper())
-    for num,definition in enumerate(cleanedDefinitions):
-        if num % 2 == 0 and args.alternating == True and args.color == True:
-        definition = colorama.Fore.CYAN + definition
-        print ("{0}. {1}".format(num+1, definition))
+        cleanedDefinitions = parseWord(word)
+        if args.color == True:
+            print(colorama.Fore.RED + colorama.Style.BRIGHT + word.upper())
+        else:
+            print(word.upper())
 
-        sys.stdout = stdout
+        for num,definition in enumerate(cleanedDefinitions):
+            if num % 2 == 0 and args.alternating == True and args.color == True:
+                definition = colorama.Fore.CYAN + definition
+            print ("{0}. {1}".format(num+1, definition))
+
+    sys.stdout = stdout
